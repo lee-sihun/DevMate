3383680fa3d76a4effc1658c167215cfde810768
wooyunha <wjh0970@gmail.com>
2023-08-16 19:02:01 +0900

    design: 글로벌 스타일링 적용 및 파비콘 설정

---
+++ src/components/common/Card/Card.styled.tsx
@@ -0,0 +1,14 @@
+import { styled } from 'styled-components';
+
+export const CardImg = styled.img`
+  
+`;
+export const CardH1 = styled.h1`
+  
+`;
+export const CardH2 = styled.h2`
+  
+`;
+export const CardSmall = styled.small`
+  
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
+import GlobalStyle from './GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -13,6 +14,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+        <GlobalStyle />
         <App />
       </Provider>
     </BrowserRouter>
