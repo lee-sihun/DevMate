0659986429468acff7bf1598d8d7c87ced055bf9
wooyunha <wjh0970@gmail.com>
2023-08-25 22:22:06 +0900

    Fix: Cors이슈 -  Proxy 서버 설정 완료

--- src/pages/Auth/SignIn.tsx
+++ src/pages/Auth/SignIn.tsx
@@ -24,8 +24,8 @@ const SignInForm = () => {
   const [singIn, { data, isLoading, isError, isSuccess }] = useSignInMutation();
 
   const onSubmit: SubmitHandler<IFormInput> = async (data) => {
-    console.log(data);
-    await singIn(data);
+    // console.log(data);
+    singIn(data);
   };
 
   useEffect(() => {
@@ -42,6 +42,7 @@ const SignInForm = () => {
           label="이메일"
           id="email"
           type="email"
+          defaultValue='yun@yun.com'
           registerOptions={register('email', {
             required: '이메일을 입력해주세요',
             pattern: {
@@ -56,6 +57,7 @@ const SignInForm = () => {
           label="비밀번호"
           id="password"
           type="password"
+          defaultValue='yun'
           registerOptions={register('password', {
             required: '비밀번호를 입력해주세요',
           })}
--- src/store/hooks/user.hooks.ts
+++ src/store/hooks/user.hooks.ts
@@ -3,7 +3,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const userApi = createApi({
   reducerPath: 'userApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.REACT_APP_API_SERVER_URL}/api/users/`,
+    baseUrl: '/api/users/',
+    // baseUrl: `${process.env.REACT_APP_API_SERVER_URL}/api/users/`,
+    credentials: 'include',
   }), // API 엔드포인트 설정
 
   endpoints: (builder) => ({
@@ -11,9 +13,6 @@ export const userApi = createApi({
       query: (signInData) => ({
         url: 'login',
         method: 'POST',
-        // headers: {
-        //   'Content-Type': 'multipart/form-data', // form-data 형식 지정
-        // },
         body: signInData,
       }),
     }),
@@ -21,10 +20,7 @@ export const userApi = createApi({
       query: (signInData) => ({
         url: 'signup',
         method: 'POST',
-        mode:'cors',
-        // headers: {
-        //   'Content-Type': 'multipart/form-data', // form-data 형식 지정
-        // },
+        mode: 'cors',
         body: signInData,
       }),
     }),
