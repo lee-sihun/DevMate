d26dfb256fdd3a3d30258e9e87d71dd67d5f1634
lee-sihun <lee.sihun040223@gmail.com>
2023-08-27 11:17:01 +0900

    commit

--- src/components/features/SearchField/SearchBar/SearchBar.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Container, InputStyled, SearchIcon } from './SearchBar.styled';
-
-const SearchBar = () => {
-  return <Container><SearchIcon /><InputStyled placeholder='검색어를 입력해주세요'/></Container>;
-};
-
-export default SearchBar;
